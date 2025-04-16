e1bdd34d6f0f5ac551f7cea3ac1d2c3a8790c554
ChibiGalaxies <trixiethecloudiewolf@gmail.com>
2025-04-16 21:44:58 +0100

    Adds ignores to analysis_options + Adds code for Appbar directly to home screen

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
 
+// import 'reusable_widgets.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(const AppBarApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class AppBarApp extends StatelessWidget {
+  const AppBarApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: HomeScreen(),);
   }
 }
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('PETNAME'),
+          actions: <Widget> [
+              Padding(padding: const EdgeInsets.only(right: 16.0),
+              child: RichText(text: TextSpan(
+                children: [
+                  WidgetSpan(child: Icon(Icons.savings)),
+                  TextSpan(text: "0", style: TextStyle(color: Colors.black, fontSize: 20)),
+                ]
+              )
+              )
+              )
+          ],
+        )
+
+      )
+    );
+}
+}
+
+// class SettingsScreen extends StatelessWidget {
+//   const SettingsScreen({super.key});
+
+// }
